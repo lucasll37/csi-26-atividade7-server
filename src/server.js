41ab90b31de69e1201254f5e21236f00a49a5b94
Lucas <lucas.silva1037@gmail.com>
2023-10-29 21:42:33 -0300

    up

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+let products = [];
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+app.post('/products', (req, res) => {
+    products.push(req.body);
+    res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
